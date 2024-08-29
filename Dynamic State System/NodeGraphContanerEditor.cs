6afc4cb6c4b57203be11122e118622cf0aa0cad6
MrGreenwud <108620747+MrGreenwud@users.noreply.github.com>
2024-08-29 04:56:40 +0400

    Add files via upload

---
+++ Dynamic State System/NodeGraphContanerEditor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Enigmatic.Experemental.ENIX;
+
+public class NodeGraphContanerEditor : ENIXContaner
+{
+    [SerializeField] private NodeGraphContanerBase m_NodeGraph;
+    public NodeGraphContanerBase NodeGraph => m_NodeGraph;
+
+    public bool TrySetNodeGraph(NodeGraphContanerBase nodeGraph)
+    {
+        if (m_NodeGraph != null)
+            return false;
+
+        if (nodeGraph == null)
+            return false;
+
+        m_NodeGraph = nodeGraph;
+        return true;
+    }
+}
